7807c9cd5d32084d7efbe24d061ef529302b4165
alan <alanpanzich@hotmail.com>
2025-03-30 18:05:46 -0300

    blue

--- js/buscador.js
+++ js/buscador.js
@@ -21,7 +21,7 @@ searchForm.addEventListener('submit', (e) => {
   ];
 
   const imagenes = {
-    "bluebeetle": "img/bluebeetle.jpg",
+    "bluebeetle": "bluebeetle.webp",
     "megalodon2": "img/megalodon2.jpg",
     "barbie": "img/barbie.jpg",
     // ...
