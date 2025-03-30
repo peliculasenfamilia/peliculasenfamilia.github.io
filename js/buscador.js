c453f6ede8e28968df9ffa52bee400a6c78bc025
alan <alanpanzich@hotmail.com>
2025-03-30 18:04:23 -0300

    a ver

--- js/buscador.js
+++ js/buscador.js
@@ -28,27 +28,32 @@ searchForm.addEventListener('submit', (e) => {
   };
   function getImagenUrl(pelicula) {
     const alt = pelicula.title.toLowerCase().replace(' ', '');
+    console.log(`Buscando imagen para ${alt}`);
     return imagenes[alt] || 'img/default.jpg'; // Ruta de la imagen por defecto si no se encuentra
   }
 
-  // Filtrar las películas y series según el valor de búsqueda
-  const filteredPeliculas = peliculas.filter((pelicula) => {
-    return pelicula.title.toLowerCase().includes(searchValue.toLowerCase());
-  });
-
-  // Mostrar las películas y series filtradas
-peliculasLista.innerHTML = '';
-filteredPeliculas.forEach((pelicula) => {
-  const imagenUrl = getImagenUrl(pelicula);
-  const peliculaHTML = `
-    <a href="${pelicula.url}">
-      <div class="div-block">
-        <div class="div-block-2"><img src="https://uploads-ssl.webflow.com/64a5fa02f0bb2c4bf6dbdf77/64a5fa02f0bb2c4bf6dbdfb4_botonplay.png" loading="lazy" alt="bodon de play" class="iconoplay" /></div>
-        <img src="${imagenUrl}" loading="lazy" alt="${pelicula.title}" class="image-2" />
-        <h3>${pelicula.title}</h3>
-      </div>
-    </a>
-  `;
-  peliculasLista.insertAdjacentHTML('beforeend', peliculaHTML);
+ // Filtrar las películas y series según el valor de búsqueda
+const filteredPeliculas = peliculas.filter((pelicula) => {
+  return pelicula.title.toLowerCase().includes(searchValue.toLowerCase());
 });
+console.log(`Encontradas ${filteredPeliculas.length} películas`);
+
+// Mostrar las películas y series filtradas
+peliculasLista.innerHTML = '';
+if (filteredPeliculas.length === 0) {
+  peliculasLista.innerHTML = '<p>No se encontraron resultados</p>';
+} else {
+  filteredPeliculas.forEach((pelicula) => {
+    const imagenUrl = getImagenUrl(pelicula);
+    const peliculaHTML = `
+      <a href="${pelicula.url}">
+        <div class="div-block">
+          <div class="div-block-2"><img src="https://uploads-ssl.webflow.com/64a5fa02f0bb2c4bf6dbdf77/64a5fa02f0bb2c4bf6dbdfb4_botonplay.png" loading="lazy" alt="bodon de play" class="iconoplay" /></div>
+          <img src="${imagenUrl}" loading="lazy" alt="${pelicula.title}" class="image-2" />
+          <h3>${pelicula.title}</h3>
+        </div>
+      </a>
+    `;
+    peliculasLista.insertAdjacentHTML('beforeend', peliculaHTML);
+  });
 });
