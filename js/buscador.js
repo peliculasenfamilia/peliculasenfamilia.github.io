e735c3fc29c6ac69f6fb17146c5d1c5068289091
alan <alanpanzich@hotmail.com>
2025-03-30 18:11:48 -0300

    original

--- js/buscador.js
+++ js/buscador.js
@@ -16,40 +16,22 @@ searchForm.addEventListener('submit', (e) => {
     { title: 'Barbie', url: 'reproductor-barbie.html' },
     { title: 'Spiderman: Across', url: 'reproductor-spidermanacross.html' },
     { title: 'Wolf Pack', url: 'reproductor-wolfpack.html' },
-    { title: 'megalodon', url: 'reproductor-wolfpack.html' },
     // Agregar más películas y series aquí...
   ];
 
-  const imagenes = {
-    "bluebeetle": "img/bluebeetle.webp",
-    "megalodon2": "img/megalodon2.jpg",
-    "barbie": "img/barbie.jpg",
-    // ...
-  };
-  function getImagenUrl(pelicula) {
-    const alt = pelicula.title.toLowerCase().replace(' ', '');
-    console.log(`Buscando imagen para ${alt}`);
-    return imagenes[alt] || 'img/default.jpg'; // Ruta de la imagen por defecto si no se encuentra
-  }
+  // Filtrar las películas y series según el valor de búsqueda
+  const filteredPeliculas = peliculas.filter((pelicula) => {
+    return pelicula.title.toLowerCase().includes(searchValue.toLowerCase());
+  });
 
- // Filtrar las películas y series según el valor de búsqueda
-const filteredPeliculas = peliculas.filter((pelicula) => {
-  return pelicula.title.toLowerCase().includes(searchValue.toLowerCase());
-});
-console.log(`Encontradas ${filteredPeliculas.length} películas`);
-
-// Mostrar las películas y series filtradas
-peliculasLista.innerHTML = '';
-if (filteredPeliculas.length === 0) {
-  peliculasLista.innerHTML = '<p>No se encontraron resultados</p>';
-} else {
+  // Mostrar las películas y series filtradas
+  peliculasLista.innerHTML = '';
   filteredPeliculas.forEach((pelicula) => {
-    const imagenUrl = getImagenUrl(pelicula);
     const peliculaHTML = `
       <a href="${pelicula.url}">
         <div class="div-block">
           <div class="div-block-2"><img src="https://uploads-ssl.webflow.com/64a5fa02f0bb2c4bf6dbdf77/64a5fa02f0bb2c4bf6dbdfb4_botonplay.png" loading="lazy" alt="bodon de play" class="iconoplay" /></div>
-          <img src="${imagenUrl}" loading="lazy" alt="${pelicula.title}" class="image-2" />
+          <img src="img/${pelicula.title.toLowerCase().replace(' ', '')}.jpg" loading="lazy" alt="${pelicula.title}" class="image-2" />
           <h3>${pelicula.title}</h3>
         </div>
       </a>
